8c662ca0da2d4cc4cf07fb280825ae393193b2e8
Jeff Karkari-Apau <jeffrey.k.apau@gmail.com>
2022-11-10 10:36:53 +0000

    Finished implementing all the options so the program is fully functional

--- task_manager.py
+++ task_manager.py
@@ -5,13 +5,18 @@ import datetime
 
 # The passw and user variables are used to store the values from the file.
 # The username and password variables are used to store the user input.
+# The all_tasks variable stores the contents of the tasks.txt file.
+# the number_of_tasks and number_of_users variables are counters store an int.
 # The set_password boolean is used for the loop that checks whether the user has entered
 # the correct password.
-# The current_date variable stores current date
+# The current_date variable stores current date.
 passw = ""
 user = ""
 username = ""
 password = ""
+all_tasks = ""
+number_of_tasks = 0
+number_of_users = 0
 set_password = True
 current_date = datetime.datetime.now()
 
@@ -31,14 +36,9 @@ with open('user.txt', 'r+') as f:
     credentials = user.split(",") 
     user = credentials[0::2]
     passw = credentials[1::2]
-
-#!!!!Remove!!!!
-print(user)
-print(passw)
-#!!!!Remove!!!!
  
  # This loop is used to repeatedly check whether the user has entered the correct username by comparing the user enters with what
- # is stored in the username variable. If they do not match the user is asked to try again
+ # is stored in the username variable. If they do not match the user is asked to try again.
 for name in user:
     username = input("\nPlease enter your username: ")
     if username in user:
@@ -48,7 +48,7 @@ for name in user:
         print("You username is incorrect, please try again.")
 
  # This loop is used to repeatedly check whether the user has entered the correct password by comparing what the user enters with what
- # is stored in the password variable. If they do not match the user is asked to try again
+ # is stored in the password variable. If they do not match the user is asked to try again.
 for passphrase in user:
     password = input("Please enter your password: ")
     if password in passw:
@@ -57,59 +57,58 @@ for passphrase in user:
     else:
         print("Your password is incorrect")
 
-'''Here you will write code that will allow a user to login.
-    - Your code must read usernames and password from the user.txt file
-    - You can use a list or dictionary to store a list of usernames and passwords from the file.
-    - Use a while loop to validate your user name and password.
-'''
-
 while True:
-    # Presenting the menu to the user and 
-    # making sure that the user input is converted to lower case.
-    menu = input('''\nSelect one of the following Options below:
-    r - Registering a user
-    a - Adding a task
-    va - View all tasks
-    vm - view my task
-    e - Exit
-    : ''').lower()
+    if username == 'admin':
+        # Presenting the menu to the user and 
+        # making sure that the user input is converted to lower case.
+        # A new menu option is added if the user has logged in as the admin user.
+        menu = input('''\nSelect one of the following Options below:
+        r - Registering a user
+        a - Adding a task
+        va - View all tasks
+        vm - view my task
+        s - View statictics
+        e - Exit
+        : ''').lower()
+    
+    else:
+        # Presenting the menu to the user and 
+        # making sure that the user input is converted to lower case.
+        menu = input('''\nSelect one of the following Options below:
+        r - Registering a user
+        a - Adding a task
+        va - View all tasks
+        vm - view my task
+        e - Exit
+        : ''').lower()
 
     # If the user selects option r in the menu the user is prompted to add a new user
     if menu == 'r':
-        # While the set_password variable is set to True, the user is asked to enter the new users, username and password. They are
-        # asked to confirm the password.
-        while set_password:
-            new_username = input("Please enter the new username: ")
-            new_password = input("Please enter the new password: ")
-            confirm_password = input("Please re-enter the new password: ")
-
-            # If both entries of the password match, the username and password are stored in a variable called new_credentials with a
-            # comma appended to the end of the username and a space before the new password to maintain the same format in the text file.
-            if confirm_password == new_password:
-                new_credentials = new_username + ","
-                new_credentials += " " + new_password
-                set_password = False
-
-                #!!!!Remove!!!!
-                print(new_credentials)
-                #!!!!Remove!!!!
-
-                # This write the new_credentials variable into the user.txt file. If the passwords do not match then the user is asked to
-                # start over from entering the username.
-                with open('user.txt', 'a') as f:
-                    f.write('\n' + new_credentials)
-            else:
-                print("The passwords do not match please try again: ")
-
+        if username != 'admin':
+            print("Sorry you must be logged in as admin to complete this action.")
+            continue
         
-        '''In this block you will write code to add a new user to the user.txt file
-        - You can follow the following steps:
-            - Request input of a new username
-            - Request input of a new password
-            - Request input of password confirmation.
-            - Check if the new password and confirmed password are the same.
-            - If they are the same, add them to the user.txt file,
-            - Otherwise you present a relevant message.'''
+        else:
+            # While the set_password variable is set to True, the user is asked to enter the new users, username and password. They are
+            # asked to confirm the password.
+            while set_password:
+                new_username = input("Please enter the new username: ")
+                new_password = input("Please enter the new password: ")
+                confirm_password = input("Please re-enter the new password: ")
+
+                # If both entries of the password match, the username and password are stored in a variable called new_credentials with a
+                # comma appended to the end of the username and a space before the new password to maintain the same format in the text file.
+                if confirm_password == new_password:
+                    new_credentials = new_username + ","
+                    new_credentials += " " + new_password
+                    set_password = False
+
+                    # This write the new_credentials variable into the user.txt file. If the passwords do not match then the user is asked to
+                    # start over from entering the username.
+                    with open('user.txt', 'a') as f:
+                        f.write('\n' + new_credentials)
+                else:
+                    print("The passwords do not match please try again: ")
 
     # If the user selects option a in the menu the user is prompted to add a new task
     elif menu == 'a':
@@ -134,42 +133,50 @@ while True:
         with open('tasks.txt', 'a') as f:
             f.write('\n' + new_task)
 
-
-        '''In this block you will put code that will allow a user to add a new task to task.txt file
-        - You can follow these steps:
-            - Prompt a user for the following: 
-                - A username of the person whom the task is assigned to,
-                - A title of a task,
-                - A description of the task and 
-                - the due date of the task.
-            - Then get the current date.
-            - Add the data to the file task.txt and
-            - You must remember to include the 'No' to indicate if the task is complete.'''
-
+    # If the user selects option va in the menu the user is shown all the tasks on the file
     elif menu == 'va':
-        pass
-        '''In this block you will put code so that the program will read the task from task.txt file and
-         print to the console in the format of Output 2 presented in the L1T19 pdf file page 6
-         You can do it in this way:
-            - Read a line from the file.
-            - Split that line where there is comma and space.
-            - Then print the results in the format shown in the Output 2 in L1T19 pdf
-            - It is much easier to read a file using a for loop.'''
 
+        # This opens the task.txt file with read/write permissions
+        with open('tasks.txt', 'r+') as f:
+            # This loop adds each line from the text file with a newline character to a variable called all_tasks
+            #  and prints all the tasks to the user
+            for line in f:
+                all_tasks += line + ("\n")
+            print(all_tasks)
+
+    # If the user selects option vm in the menu the user is shown all the tasks assigned to them from the file
     elif menu == 'vm':
-        pass
-        '''In this block you will put code the that will read the task from task.txt file and
-         print to the console in the format of Output 2 presented in the L1T19 pdf
-         You can do it in this way:
-            - Read a line from the file
-            - Split the line where there is comma and space.
-            - Check if the username of the person logged in is the same as the username you have
-            read from the file.
-            - If they are the same you print the task in the format of output 2 shown in L1T19 pdf '''
+        # This opens the task.txt file with read/write permissions
+        with open('tasks.txt', 'r+') as f:
+             # This loop adds each line from the text file to a variable called my_task
+            #  If the the current user's username is in the current line of the loop that
+            for line in f:
+                if username in line:
+                    print(line)
+    
+    # If the admin selects option s in the menu the user is shown the count of all users and all tasks
+    elif menu == 's':
+        # This opens the task.txt file with read/write permissions
+        with open('tasks.txt', 'r+') as f:
+            # This loop will add 1 to the number_of_tasks variable for every line in the tasks.txt
+            for line in f:
+                number_of_tasks += 1
+        
+        # This opens the task.txt file with read/write permissions
+        with open('user.txt', 'r+') as f:
+            # This loop will add 1 to the number_of_users variable for every line in the tasks.txt
+            for line in f:
+                number_of_users += 1
+        
+        # This will print the total amount of tasks and users
+        print(f"\nThere are {number_of_users} users and {number_of_tasks} tasks.")
 
+    # If the user selects option e in the menu the goodbye message is displayed and the program ends
     elif menu == 'e':
         print('Goodbye!!!')
         exit()
 
+    # If at the selection stage the user hasn't selected an option from the menu but entered something else they are prompted
+    # to try again and the loop interates
     else:
         print("You have made a wrong choice, Please Try again")
